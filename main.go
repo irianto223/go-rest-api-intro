52739e36185926f47cd9a0221da284195ec1bdce
Irianto <irianto223@gmail.com>
2019-05-28 03:16:30 +0700

    http, route with mux

---
+++ main.go
@@ -0,0 +1,85 @@
+package main
+
+import (
+	"encoding/json"
+	"log"
+	"math/rand"
+	"net/http"
+	"strconv"
+
+	"github.com/gorilla/mux"
+)
+
+// Book struct (Model)
+type Book struct {
+	ID     string  `json:"id"`
+	Isbn   string  `json:"isbn"`
+	Title  string  `json:"title"`
+	Author *Author `json:"author"`
+}
+
+type Author struct {
+	Firstname string `json:"firstname"`
+	Lastname  string `json:"lastname"`
+}
+
+// Init books variable as a slice Book struct
+var books []Book
+
+// Get all books
+func getBooks(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+	json.NewEncoder(w).Encode(books)
+}
+
+// Get a book
+func getBook(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+	params := mux.Vars(r) // Get params
+	// Loop through books and find with id
+	for _, item := range books {
+		if item.ID == params["id"] {
+			json.NewEncoder(w).Encode(item)
+			return
+		}
+	}
+	json.NewEncoder(w).Encode(&Book{})
+}
+
+// Create a book
+func createBook(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+	var book Book
+	_ = json.NewDecoder(r.Body).Decode(&book)
+	book.ID = strconv.Itoa(rand.Intn(10000000)) // Mock ID - not safe
+	books = append(books, book)
+	json.NewEncoder(w).Encode(book)
+}
+
+// Update a book
+func updateBook(w http.ResponseWriter, r *http.Request) {
+
+}
+
+// Delete a book
+func deleteBook(w http.ResponseWriter, r *http.Request) {
+
+}
+
+func main() {
+	//  Init router
+	r := mux.NewRouter()
+
+	// Mock data - @todo - implement DB
+	books = append(books, Book{ID: "1", Isbn: "87879", Title: "Book One", Author: &Author{Firstname: "John", Lastname: "Albab"}})
+	books = append(books, Book{ID: "2", Isbn: "87880", Title: "Book Two", Author: &Author{Firstname: "Ulil", Lastname: "Mayer"}})
+
+	// Route handlers / Endpoints
+	r.HandleFunc("/api/books", getBooks).Methods("GET")
+	r.HandleFunc("/api/books/{id}", getBook).Methods("GET")
+	r.HandleFunc("/api/books", createBook).Methods("POST")
+	r.HandleFunc("/api/books/{id}", updateBook).Methods("PUT")
+	r.HandleFunc("/api/books", deleteBook).Methods("DELETE")
+
+	log.Fatal(http.ListenAndServe(":8000", r))
+}
